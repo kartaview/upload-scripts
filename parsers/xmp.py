8a518f02727342a4b520166e44815b878124240c
Bogdan Sala <sala.bogdan@gmail.com>
2022-03-01 15:04:52 +0200

    Added width, height reading from image exif headers. (#122)
    * feat: Implemented get my images functionality
    * chore: fixed ussless spaces and incorrect project naming
    * feat: Added width and height reading from image exif. 
    * feat: capability to write XMP headers for JPEG images.
    * chore: ignored long line warning for specific case.
    Co-authored-by: Bogdan Sala <bogdan.sala@grab.com>

--- parsers/xmp.py
+++ parsers/xmp.py
@@ -1,12 +1,13 @@
 """
 This module is used to read XMP data from images.
 """
+import struct
 from typing import Optional, Tuple, List, Any, Type
 from xml.etree.ElementTree import fromstring, ParseError
 
 from io_storage.storage import Storage
 from parsers.base import BaseParser
-from common.models import SensorItem, CameraParameters, projection_type_from_name
+from common.models import SensorItem, CameraParameters, projection_type_from_name, ExifParameters
 
 
 class XMPParser(BaseParser):
@@ -24,6 +25,7 @@ class XMPParser(BaseParser):
             xmp_start = data.find(b'<x:xmpmeta')
             xmp_end = data.find(b'</x:xmpmeta')
             xmp_str = data[xmp_start:xmp_end + 12]
+
         return xmp_str
 
     def next_item_with_class(self, item_class: Type[SensorItem]) -> Optional[SensorItem]:
@@ -50,10 +52,6 @@ class XMPParser(BaseParser):
 
     def format_version(self) -> Optional[str]:
         raise NotImplementedError(f"XMP format version - {self}")
-
-    def serialize(self):
-        """this method will write all the added items to file"""
-        raise NotImplementedError(f"XMP serialize is not implemented - {self}")
 
     @classmethod
     def compatible_sensors(cls) -> List[Any]:
@@ -119,3 +117,27 @@ class XMPParser(BaseParser):
                 projection = xml_child.text
 
         return full_pano_image_width, cropped_area_image_width_pixels, projection
+
+    def serialize(self):
+        with self._storage.open(self.file_path, "rb") as image:
+            data = image.read()
+            start = data.find(b'\xff\xe1')
+            for item in self._sensors:
+                if isinstance(item, ExifParameters):
+                    height = item.height
+                    width = item.width
+                    # print(str(hex_val) + xmp_header)
+            # pylint: disable=C0301
+            xmp_header = '''<x:xmpmeta xmlns:x="adobe:ns:meta/" x:xmptk="Adobe XMP Core 5.1.0-jc003">\n<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">\n<rdf:Description rdf:about="" xmlns:GPano="http://ns.google.com/photos/1.0/panorama/">\n<GPano:UsePanoramaViewer>True</GPano:UsePanoramaViewer>\n<GPano:ProjectionType>equirectangular</GPano:ProjectionType>\n<GPano:PoseHeadingDegrees>0.0</GPano:PoseHeadingDegrees>\n<GPano:PosePitchDegrees>0.0</GPano:PosePitchDegrees>\n<GPano:PoseRollDegrees>0.0</GPano:PoseRollDegrees>\n<GPano:InitialViewHeadingDegrees>0.0</GPano:InitialViewHeadingDegrees>\n<GPano:InitialViewPitchDegrees>0.0</GPano:InitialViewPitchDegrees>\n<GPano:InitialViewRollDegrees>0.0</GPano:InitialViewRollDegrees>\n<GPano:InitialHorizontalFOVDegrees>0.0</GPano:InitialHorizontalFOVDegrees>\n<GPano:InitialVerticalFOVDegrees>0.0</GPano:InitialVerticalFOVDegrees>\n<GPano:CroppedAreaLeftPixels>0</GPano:CroppedAreaLeftPixels>\n<GPano:CroppedAreaTopPixels>0</GPano:CroppedAreaTopPixels>\n<GPano:CroppedAreaImageWidthPixels>{imagewidth}</GPano:CroppedAreaImageWidthPixels>\n<GPano:CroppedAreaImageHeightPixels>{imageheight}</GPano:CroppedAreaImageHeightPixels>\n<GPano:FullPanoWidthPixels>{imagewidth}</GPano:FullPanoWidthPixels>\n<GPano:FullPanoHeightPixels>{imageheight}</GPano:FullPanoHeightPixels>\n</rdf:Description>\n</rdf:RDF>\n</x:xmpmeta>'''.format(imageheight=height, imagewidth=width)
+            # pylint: enable=C0301
+            string_len = len(xmp_header.encode('utf-8')) + 2
+            xmp_header = b'\xff\xe1' + struct.pack('>h', string_len) + xmp_header.encode('utf-8')
+            if start == -1:
+                with self._storage.open(self.file_path, "wb") as out_image:
+                    out_image.write(data[:2] + xmp_header + data[2:])
+            elif len(self.xmp_str) > 0:
+                NotImplementedError("Adding information to existing XMP header is currently "
+                                    "not supported")
+            else:
+                with self._storage.open(self.file_path, "wb") as out_image:
+                    out_image.write(data[:start] + xmp_header + data[start:])
